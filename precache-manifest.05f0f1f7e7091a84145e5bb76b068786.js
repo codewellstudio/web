894b80c8327bfdeb43910e3dd30aa6c57d78a174
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2023-09-14 19:15:47 +0200

    Updates

---
+++ precache-manifest.05f0f1f7e7091a84145e5bb76b068786.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507e55e3e33c595eb9a9846e514de931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "545aefb78dd4fa2dd94c",
+    "url": "/static/css/main.1e87757d.chunk.css"
+  },
+  {
+    "revision": "77ccd2006f0b2262dad3",
+    "url": "/static/js/2.71481ff1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.71481ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545aefb78dd4fa2dd94c",
+    "url": "/static/js/main.74824d07.chunk.js"
+  },
+  {
+    "revision": "922c46902b87d706df89",
+    "url": "/static/js/runtime-main.d89cee0a.js"
+  },
+  {
+    "revision": "955965d8352207746b95def76970da92",
+    "url": "/static/media/credit-karma-project.955965d8.webp"
+  },
+  {
+    "revision": "bc0a37536d2037f7f63f406a3c6162ba",
+    "url": "/static/media/credit-karma-project.bc0a3753.jpg"
+  },
+  {
+    "revision": "88fd2e9acd7fe77413a1161108fee112",
+    "url": "/static/media/glean-compare.88fd2e9a.webp"
+  },
+  {
+    "revision": "bd68ff798f51a4b065eaf472b7d174d5",
+    "url": "/static/media/glean-compare.bd68ff79.jpg"
+  },
+  {
+    "revision": "4eb7b071a9958c35233c3706703e90f6",
+    "url": "/static/media/glean-homepage.4eb7b071.webp"
+  },
+  {
+    "revision": "7692532f6173a005b42eb986b819df89",
+    "url": "/static/media/glean-homepage.7692532f.jpg"
+  },
+  {
+    "revision": "156d35bdd8c1007201816e552022f0dd",
+    "url": "/static/media/glean-payment-1.156d35bd.jpg"
+  },
+  {
+    "revision": "de966d0b588248e436eb816f77be7749",
+    "url": "/static/media/glean-payment-1.de966d0b.webp"
+  },
+  {
+    "revision": "785f1704d6a6f557f6ce681a72501139",
+    "url": "/static/media/glean-payment-2.785f1704.webp"
+  },
+  {
+    "revision": "b9e45ef94fd7b727065545e13f3e808c",
+    "url": "/static/media/glean-payment-2.b9e45ef9.jpg"
+  },
+  {
+    "revision": "86ddc3bde94712c4ace385aaf6898ccd",
+    "url": "/static/media/glean-project.86ddc3bd.jpg"
+  },
+  {
+    "revision": "cf27438074e026ea536cdf3914d5fa3e",
+    "url": "/static/media/glean-project.cf274380.webp"
+  },
+  {
+    "revision": "f7adb7a8aa98b57c52c63baf54f6edf4",
+    "url": "/static/media/logo.f7adb7a8.svg"
+  },
+  {
+    "revision": "1ecee744830294baf55eb5d1254e0ba1",
+    "url": "/static/media/movieo-project.1ecee744.jpg"
+  },
+  {
+    "revision": "1f656adea0961fbcd0032aca4ac09666",
+    "url": "/static/media/movieo-project.1f656ade.webp"
+  },
+  {
+    "revision": "0005d8866ba6fce0c8bc126acef98d14",
+    "url": "/static/media/solarisbank-project.0005d886.jpg"
+  },
+  {
+    "revision": "0bdef992b07b613efcb48114ca92ebb9",
+    "url": "/static/media/solarisbank-project.0bdef992.webp"
+  },
+  {
+    "revision": "9bccfe89e3831417467b855c39ef19d1",
+    "url": "/static/media/tech-logos.9bccfe89.png"
+  }
+]);
